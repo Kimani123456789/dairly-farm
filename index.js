dfd1af8237ac07cc18462839d068afe70ce72010
Moringa School <machariad196@protonmail.com>
2021-03-13 23:59:56 +0300

    function to calculate income

--- index.js
+++ index.js
@@ -21,3 +21,25 @@ let dailyLtrsTotal = shedADaily +
 let dailyIncome = dailyLtrsTotal * 45;
 
 //Calculating daily income
+
+let dailyLtrsTotal = shedADaily +
+    shedBDaily + shedCDaily + shedDDaily;
+
+let dailyIncome = dailyLtrsTotal * 45;
+
+//Calculating daily income
+
+
+//Independent Variable
+let annualDays = 365;
+//Independent Variable
+
+//Milk produced per shed report
+function totalProduction() {
+    let milkOutPut = `The production in Shed A is ${shedADaily} litres per day.
+    The production in Shed B is ${shedBDaily} litres per day.
+    The production in Shed C is ${shedCDaily} litres per day.
+    The production in Shed D is ${shedDDaily} litres per day.
+    The total milk production is ${dailyLtrsTotal} litres per day.`;
+    document.getElementById('milkpershedoutput').innerText = milkOutPut;
+};
