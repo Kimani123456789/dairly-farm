675304c00471852f9d571bc10c4e5441afbbae7d
Moringa School <machariad196@protonmail.com>
2021-03-14 00:16:33 +0300

    calculating weekly and anual production

--- index.js
+++ index.js
@@ -14,32 +14,66 @@ const shedDDaily = dailyMilkProduction(52, 11);
 
 
 //Calculating daily income
-
-let dailyLtrsTotal = shedADaily +
+    let dailyLtrsTotal = shedADaily +
     shedBDaily + shedCDaily + shedDDaily;
 
-let dailyIncome = dailyLtrsTotal * 45;
-
-//Calculating daily income
-
-let dailyLtrsTotal = shedADaily +
+    let dailyIncome = dailyLtrsTotal * 45;
+    
+    //Calculating daily income
+    let dailyLtrsTotal = shedADaily +
     shedBDaily + shedCDaily + shedDDaily;
 
-let dailyIncome = dailyLtrsTotal * 45;
+    let dailyIncome = dailyLtrsTotal * 45;
+    
+    
+    //Independent Variable
+    let annualDays = 365;
+    //Independent Variable
+    
+    //Milk produced per shed report
+    function totalProduction() {
+        let milkOutPut = `The production in Shed A is ${shedADaily} litres per day.
+        The production in Shed B is ${shedBDaily} litres per day.
+        The production in Shed C is ${shedCDaily} litres per day.
+        The production in Shed D is ${shedDDaily} litres per day.
+        The total milk production is ${dailyLtrsTotal} litres per day.`;
+        document.getElementById('milkpershedoutput').innerText = milkOutPut;
+    };
+    
+    totalProduction();
+    //Milk produced per shed report
+    
+    //Weekly and yearly income report
+    function incomeOverTime(buyingRate, time) {
+    
+        let weeklyIncome = buyingRate * time * dailyLtrsTotal;
+        let yearlyIncome = dailyIncome * annualDays;
+    
+        let weeklyReport = `The daily income will be Ksh.${dailyIncome}
+    The weekly income will be Ksh.${weeklyIncome}
+    The yearly income will be Ksh.${yearlyIncome}`;
+    
+        document.getElementById('weekreportsoutput').innerText = weeklyReport;
+    };
+    
+    incomeOverTime(45, 7);
+    //Weekly and yearly income report
+    
+    
+    let monthlyData = {
+        january: 31,
+        february: 29,
+        march: 31,
+        april: 30,
+        may: 31,
+        june: 30,
+        july: 31,
+        august: 31,
+        sept: 30,
+        oct: 31,
+        nov: 30,
+        dec: 31
+    };
 
-//Calculating daily income
 
 
-//Independent Variable
-let annualDays = 365;
-//Independent Variable
-
-//Milk produced per shed report
-function totalProduction() {
-    let milkOutPut = `The production in Shed A is ${shedADaily} litres per day.
-    The production in Shed B is ${shedBDaily} litres per day.
-    The production in Shed C is ${shedCDaily} litres per day.
-    The production in Shed D is ${shedDDaily} litres per day.
-    The total milk production is ${dailyLtrsTotal} litres per day.`;
-    document.getElementById('milkpershedoutput').innerText = milkOutPut;
-};
